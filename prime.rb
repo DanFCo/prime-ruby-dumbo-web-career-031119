a743e06af5b353e458303133abb7712d2a90c7b5
DanFCo <danfcorreia@gmail.com>
2019-02-14 21:04:34 -0500

    prime ruby

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+def prime?(n)
+
+  if n > 1 && n % n == 0 && n / n == 1
+    factors = (2..(n-1)).to_a
+    factors.each do |numbers|
+      if  n % numbers == 0
+        return false
+      end
+    end
+    return true
+  else
+    return false
+  end
+end
+
+
+
+
 # Add  code here!
